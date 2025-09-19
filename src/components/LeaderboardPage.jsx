6610e760ee9e7530733a1125993ad3421c2c30bd
ShivamMMMxyz <kushwahashivam8037@gmail.com>
2025-09-19 09:24:06 +0530

    leaderboard spelling

--- src/components/LeaderboardPage.jsx
+++ src/components/LeaderboardPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLanguage } from '../context/LanguageContext';
-import './LeaderboardPage.css';
+import './Leaderboardpage.css';
 import edugameLogo from '../assets/edugame-logo.png'; // Placeholder for the EduGame logo
 
 // Placeholder images for top players
